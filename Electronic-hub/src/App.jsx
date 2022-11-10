4d6c8651b9fa3e43ffa22500fe6438853bb82980
+ <+>
2022-11-10 12:46:12 +0300

    orderdetails component created

--- Electronic-hub/src/App.jsx
+++ Electronic-hub/src/App.jsx
@@ -6,6 +6,8 @@ import BuyerSign from "./components/sign/BuyerSign";
 import Home from "./components/Home/Home";
 import Product from "./components/Products/Product";
 import AddProduct from "./components/Admin/AddProduct";
+import Cart from "./components/Cart/Cart";
+//import OrderDetails from "./components/Orders/OrderDetails";
 const App = () => {
   return (
     <Routes>
@@ -14,6 +16,7 @@ const App = () => {
       <Route path="/admin/add-product" element={<AddProduct />} />
       <Route path="/buyer/sign" element={<BuyerSign />} />
       <Route path="/product" element={<Product />} />
+      <Route path="/cart" element ={<Cart />} />
     </Routes>
   );
 };
