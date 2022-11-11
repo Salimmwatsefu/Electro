38667ae2d8468333095d4e917691137a36536ae4
+ <+>
2022-11-11 06:24:35 +0300

    deleted order and cart components

--- Electronic-hub/src/App.jsx
+++ Electronic-hub/src/App.jsx
@@ -7,7 +7,6 @@ import Home from "./components/Home/Home";
 import Product from "./components/Products/Product";
 import AddProduct from "./components/Admin/AddProduct";
 import Cart from "./components/Cart/Cart";
-//import OrderDetails from "./components/Orders/OrderDetails";
 const App = () => {
   return (
     <Routes>
@@ -16,7 +15,6 @@ const App = () => {
       <Route path="/admin/add-product" element={<AddProduct />} />
       <Route path="/buyer/sign" element={<BuyerSign />} />
       <Route path="/product" element={<Product />} />
-      <Route path="/cart" element ={<Cart />} />
     </Routes>
   );
 };
